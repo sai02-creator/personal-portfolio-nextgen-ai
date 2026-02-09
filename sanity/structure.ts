1fbdcbefce8e06a4e813b9e7295f20090a69e5c9
Sai Chaitanya Gaddam <saichithanya1@gmail.com>
2026-02-10 01:13:05 +1100

    feat(sanity): configure Profile as singleton in Sanity desk structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,34 @@
-import type {StructureResolver} from 'sanity/structure'
+import {
+  AsteriskIcon,
+  BookIcon,
+  CaseIcon,
+  CogIcon,
+  CommentIcon,
+  ComposeIcon,
+  DocumentIcon,
+  DocumentsIcon,
+  InlineIcon,
+  ProjectsIcon,
+  RocketIcon,
+  StarIcon,
+  TagIcon,
+  UserIcon,
+} from "@sanity/icons";
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Portfolio Content")
+    .items([
+      // Profile (Singleton)
+      S.listItem()
+        .title("Profile")
+        .icon(UserIcon)
+        .child(
+          S.document().schemaType("profile").documentId("singleton-profile"),
+        ),
+
+      S.divider(),
+
+    ])
