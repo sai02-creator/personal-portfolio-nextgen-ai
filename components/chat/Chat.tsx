ebd44911b6c1d576edd0ca69085a65bd2702fd20
Sai Chaitanya Gaddam <saichithanya1@gmail.com>
2026-02-16 01:05:46 +1100

    feat: add Chat component with typed profile prop

---
+++ components/chat/Chat.tsx
@@ -0,0 +1,8 @@
+import { CHAT_PROFILE_QUERYResult } from "@/sanity.types";
+import React from "react";
+
+function Chat({ profile }: { profile: CHAT_PROFILE_QUERYResult | null }) {
+  return <div>Chat</div>;
+}
+
+export default Chat;
